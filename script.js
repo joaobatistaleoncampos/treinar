2fef457cca18ce7653d6a2d5860bdfefa689d442
joaobatistaleoncampos <instrutorjoaoleon@gmail.com>
2025-12-01 19:32:49 -0300

    alterção javascript

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 const velocidade = document.getElementById('velocidade')
 
-const res=document.getElementById('res')
+const res = document.getElementById('res')
 
 
- function radar(){
-const vel = velocidade.value;
+function radar() {
+    const vel = velocidade.value;
 
 
 
-if(vel >40 &&vel< 50){
-    res.innerHTML+='Multado!!';
-    
+    if (vel > 40 && vel < 50) {
+        res.innerHTML += 'Multado! infração: MÉDIA';
+        res.style.color = 'red';
+    }
+    else if (vel >= 50 && vel < 60) {
+        res.innerHTML += "Multado! Infração: GRAVE"
+        res.style.color = 'red';
+    }
+
+    else {
+        res.innerHTML += 'Não multado';
+        res.style.color = 'blue';
+    }
+
 }
-
-else{
-    res.innerHTML+='Não multado'
-    
-}
-
- }
